b3f001eca09ff92ef44b00adbcb4a3c87f1ce2b8
Calebm5577 <calebm5577@gmail.com>
2020-06-23 17:01:06 -0400

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,10 +35,21 @@ module.exports = {
         extensions: ['.md', '.mdx'],
         gatsbyRemarkPlugins: [
           {
-            resolve: `gatsby-remark-images`
-          }
-        ]
-      }
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    resolve: `gatsby-plugin-google-fonts`,
+    options: {
+      fonts: [
+        `roboto mono`,
+        `muli\:400,400i,700,700i`,
+      ],
+      display: "swap"
     }
   ],
 }
